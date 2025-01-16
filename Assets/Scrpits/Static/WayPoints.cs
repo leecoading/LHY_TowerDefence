4809810639ba0530854f07e7947be9d47222006f
leecoading <leehy011011@gmail.com>
2025-01-17 00:05:52 +0900

    [Add] WayPoints

---
+++ Assets/Scrpits/Static/WayPoints.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WayPoints : MonoBehaviour
+{
+    [SerializeField] static Transform[] points;
+
+    private void Awake()
+    {
+        points = new Transform[transform.childCount];
+        for (int i = 0; i < points.Length; i++)
+        {
+            transform.GetChild(i);
+        }
+    }
+}
